c3f3fab61942ec6df560cee6dd6f9d7d64669a05
Niklas Casaril <niklas@casaril.com>
2019-09-25 07:42:56 +1000

    New targets and scripts for uwb-core and uwb-apps

--- uwb-core-setup.sh
+++ uwb-core-setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -x
 
 # Manually install the apps repo to avoid circular references
-git clone https://github.com/decawave/mynewt-dw1000-apps repos/mynewt-dw1000-apps
-cd repos/mynewt-dw1000-apps
+#git clone https://github.com/decawave/uwb-apps repos/decawave-uwb-apps
+git clone git@github.com:Decawave/uwb-apps.git
+cd repos/decawave-uwb-apps
 # Try to checkout the same branch as core from apps if it exists
 git checkout -q ${TRAVIS_BRANCH}
 echo -n "Apps branch:"
