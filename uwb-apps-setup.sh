a1369a4baf39bf42265966e33b3bbe9686c753f3
Niklas Casaril <niklas@casaril.com>
2020-07-01 15:08:55 +1000

    Manual add of dw3000 repo to uwb-apps-project.yml

--- uwb-apps-setup.sh
+++ uwb-apps-setup.sh
@@ -6,9 +6,11 @@ git checkout -q ${TRAVIS_BRANCH}
 echo -n "Core branch:"
 git status -bs
 cd -
+
 # Apply Patches
 cd repos/apache-mynewt-core
 find ../decawave-uwb-core/patches/apache-mynewt-core/ -name "mynewt_1_7_0*"|while read name;do
     git apply $name;
 done
 cd -
+
