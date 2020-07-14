b09c5849640660609188fd983a62c77918b876eb
Niklas Casaril <niklas@casaril.com>
2020-07-14 13:12:11 +1000

    Updating branch on drivers as well. Checking version of apache core before applying patches

--- uwb-apps-setup.sh
+++ uwb-apps-setup.sh
@@ -1,16 +1,31 @@
 #!/bin/bash -x
 
-cd repos/decawave-uwb-core
-# Try to checkout the same branch as core from apps if it exists
-git checkout -q ${TRAVIS_BRANCH}
-echo -n "Core branch:"
-git status -bs
-cd -
+if (( "${TRAVIS_BRANCH}" != "master" ));then
+    cd repos/decawave-uwb-core
+    # Try to checkout the same branch as core from apps if it exists
+    git checkout -q ${TRAVIS_BRANCH}
+    echo -n "Core branch:"
+    git status -bs
+    cd -
 
-# Apply Patches
+    # Try to checkout the same branch as apps/core from drivers too
+    if [ -d repos/decawave-uwb-dw1000 ];then
+        cd repos/decawave-uwb-dw1000
+        git checkout -q ${TRAVIS_BRANCH}
+        cd -
+    fi
+
+    if [ -d repos/decawave-uwb-dw3000-c0 ];then
+        cd repos/decawave-uwb-dw3000-c0
+        git checkout -q ${TRAVIS_BRANCH}
+        cd -
+    fi
+fi
+
+# Apply relevant patches to mynewt core
 cd repos/apache-mynewt-core
-find ../decawave-uwb-core/patches/apache-mynewt-core/ -name "mynewt_1_7_0*"|while read name;do
+MYNEWT_CORE_VERSION=$(git status |head -1|sed -e "s/HEAD detached at //"  -e "s/_tag//")
+find ../../patches/apache-mynewt-core/ -name "${MYNEWT_CORE_VERSION}*" | while read name;do
     git apply $name;
 done
 cd -
-
--- uwb-core-setup.sh
+++ uwb-core-setup.sh
@@ -8,9 +8,26 @@ git checkout -q ${TRAVIS_BRANCH}
 echo -n "Apps branch:"
 git status -bs
 cd -
-# Apply Patches
+
+# Try to checkout the same branch as core from drivers too
+if (( "${TRAVIS_BRANCH}" != "master" ));then
+    if [ -d repos/decawave-uwb-dw1000 ];then
+        cd repos/decawave-uwb-dw1000
+        git checkout -q ${TRAVIS_BRANCH}
+        cd -
+    fi
+
+    if [ -d repos/decawave-uwb-dw3000-c0 ];then
+        cd repos/decawave-uwb-dw3000-c0
+        git checkout -q ${TRAVIS_BRANCH}
+        cd -
+    fi
+fi
+
+# Apply relevant patches to mynewt core
 cd repos/apache-mynewt-core
-find ../../patches/apache-mynewt-core/ -name "mynewt_1_7_0*"|while read name;do
+MYNEWT_CORE_VERSION=$(git status |head -1|sed -e "s/HEAD detached at //"  -e "s/_tag//")
+find ../../patches/apache-mynewt-core/ -name "${MYNEWT_CORE_VERSION}*" | while read name;do
     git apply $name;
 done
 cd -
